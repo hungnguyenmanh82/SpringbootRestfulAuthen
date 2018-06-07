f0a514041b6248d8b5d67fbe96eb11664449c5e4
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2018-06-07 15:38:53 +0700

    springboot restful and authentication

---
+++ src/main/java/com/example/demo/SpringbootRestfulAuthenApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://o7planning.org/en/11649/secure-spring-boot-restful-service-using-basic-authentication
+ *
+ */
+@SpringBootApplication
+public class SpringbootRestfulAuthenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulAuthenApplication.class, args);
+	}
+}
